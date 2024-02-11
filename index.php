b8792d71137208d5d5bd5be92c657e5bcbad3acd
halsmur <halsmur@gmail.com>
2024-02-11 22:49:32 +0300

    stripe payment

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+  <head>
+  <title>Project</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.css">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <H1>stripe payment with php</H1>
+  <form method="post" action="checkout.php">
+
+  <p>
+    T-shirt
+  </p>
+<p>
+ <strong>
+  US$20.00
+ </strong> 
+</p>
+<button>Pay</button>
+</form>
+
+</body>
+</html>
